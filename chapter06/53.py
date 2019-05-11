4d70f3f4dbee057d0943d8a0c0a88015079dabdb
nagatech <cheesecakebot@yahoo.co.jp>
2019-05-11 16:53:05 +0900

    init chapter6

---
+++ chapter06/53.py
@@ -0,0 +1,3 @@
+#%% [markdown]
+# ## 53. Tokenization
+# Stanford Core NLPを用い，入力テキストの解析結果をXML形式で得よ．また，このXMLファイルを読み込み，入力テキストを1行1単語の形式で出力せよ．
---
+++ chapter06/57.py
@@ -0,0 +1,3 @@
+#%% [markdown]
+# ## 57. 係り受け解析
+# Stanford Core NLPの係り受け解析の結果（collapsed-dependencies）を有向グラフとして可視化せよ．可視化には，係り受け木をDOT言語に変換し，Graphvizを用いるとよい．また，Pythonから有向グラフを直接的に可視化するには，pydotを使うとよい．
