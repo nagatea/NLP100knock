3e89945a080ede558584e253cfc81f89639fa58d
nagatech <cheesecakebot@yahoo.co.jp>
2019-05-13 18:10:28 +0900

    add 54-55

--- chapter06/54.py
+++ chapter06/54.py
@@ -1,3 +1,18 @@
 #%% [markdown]
 # ## 54. 品詞タグ付け
 # Stanford Core NLPの解析結果XMLを読み込み，単語，レンマ，品詞をタブ区切り形式で出力せよ．
+
+#%%
+import xml.etree.ElementTree as ET
+root = ET.parse('nlp.txt.xml')
+
+res = []
+for token in root.iter('token'):
+  words = list(map(lambda i: token[i].text, [0,1,4]))
+  res.append('\t'.join(words))
+
+res
+
+#%%
+with open('54.txt', 'w') as f:
+  f.write('\n'.join(res))
--- chapter06/55.py
+++ chapter06/55.py
@@ -1,3 +1,14 @@
 #%% [markdown]
 # ## 55. 固有表現抽出
 # 入力文中の人名をすべて抜き出せ．
+
+#%%
+import xml.etree.ElementTree as ET
+root = ET.parse('nlp.txt.xml')
+
+res = []
+for token in root.iter('token'):
+    if token[5].text == "PERSON":
+        res.append(token[0].text)
+
+res
